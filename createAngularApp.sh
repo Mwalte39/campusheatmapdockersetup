4d2f30eb23d70426a7b43b7b115ffa4daff0f287
Mwalte39 <36418462+Mwalte39@users.noreply.github.com>
2019-09-29 22:49:37 -0400

    Update createAngularApp.sh

--- createAngularApp.sh
+++ createAngularApp.sh
@@ -5,16 +5,20 @@ then
 echo 'PLEASE INSTALL GIT AND RUN ./createAngularApp.sh again'
     exit 1
 fi
+echo 'Proceeding'
+
+read -p "Enter branchname: " branch
 
 echo 'Download and Installing UI'
 mkdir UI
 wget https://raw.githubusercontent.com/Mwalte39/campusheatmapdockersetup/master/Dockerfile
 cd UI
-git clone --single-branch --branch remove-private-vars https://github.com/danielhirt/ui-4155
+git clone --single-branch --branch "$branch" https://github.com/danielhirt/ui-4155
 cd ../
-docker build . -t 4155ui:0.2
+docker build . -t "4155ui$branch":0.2
 docker run -d \
   --name campusHeatMapUI \
   -p 4200:80 \
   --net=dockernet \
-  4155ui:0.2 
+  "4155ui$branch":0.2 
+
