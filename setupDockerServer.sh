22a5956eb7bb23b3d2ac01977eee08884a6721cf
Mwalte39 <36418462+Mwalte39@users.noreply.github.com>
2019-11-01 21:03:09 -0400

    Update setupDockerServer.sh

--- setupDockerServer.sh
+++ setupDockerServer.sh
@@ -9,6 +9,7 @@ wget https://raw.githubusercontent.com/Mwalte39/campusheatmapdockersetup/master/
 echo 'Pulling and Running INFLUXDB'
 docker run -d \
   --name influxdb \
+  --name grafana \
   --net=dockernet \
   -p 8083:8083 -p 8086:8086 \
   -v $PWD/influxdb:/var/lib/influxdb \
@@ -24,58 +25,19 @@ mkdir grafana
 chown 472:472 grafana
 docker run -d \
   --name grafana \
+  --restart always \
   --net=dockernet \
   -p 3000:3000 \
   -v $PWD/grafana:/var/lib/grafana \
   --link influxdb \
-  grafana/grafana:3.1.1
+  grafana/grafana:latest
 echo 'Grafana Starting'
 sleep 30s
 echo 'Grafana Started'
 echo 'Adding Admin for Influx'
 docker restart influxdb
 echo 'influxdb created with username and password admin CHANGE THIS ASAP'
-echo 'Pulling and Running COUCHBASEDB'
-docker run -d \
-  --net=dockernet \
-  --name couchbasedb \
-  -p 8091-8094:8091-8094 \
-  -p 11210:11210 \
-  -v $PWD/couchbase:/opt/couchbase/var\
-  couchbase
-echo 'COUCHBASEDB Starting'
-sleep 30s
 
-
-varip="$(ip route get 1 | awk '{print $NF;exit}')"
-
-echo $varip
-
-curl -u Administrator:password -v -X POST \
-http://varip:8091/node/controller/setupServices \
--d 'services=kv%2Cn1ql%2Cindex'
-
-curl -v -X POST \
-http://$varip:8091/nodes/self/controller/settings \
--d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&index_path= \
-%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata'
-
-curl -v -X POST \
-http://$varip:8091/settings/web \
--d 'password=password&username=Administrator&port=SAME'
-
-curl -u Administrator:password -v -X POST \
-http://$varip:8091/pools/default/buckets \
--d 'flushEnabled=1&threadsNumber=3&replicaIndex=0&replicaNumber=0&evictionPolicy= \
-valueOnly&ramQuotaMB=597&bucketType=membase&name=default&authType=sasl&saslPassword='
-
-curl -u Administrator:password -X POST  \
-http://$varip:8091/pools/default \
--d 'memoryQuota=1024' -d 'indexMemoryQuota=256'
-
-echo 'COUCHBASEDB created with username Administrator and password password CHANGE THIS ASAP'
-
-echo 'COUCHBASEDB Started'
 echo 'Executing createAngularApp.sh'
 chmod 777 ./createAngularApp.sh
 ./createAngularApp.sh
